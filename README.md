aa0f7758b0afad012bc7c840870c3991763d6a97
Alec Wright <alec@intrica.net>
2016-09-22 16:22:17 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,79 @@
+# Watermark
+
+This package was created to allow watermarking of various file types.
+
+### Features
+- 9 watermark positions
+- 2 types of watermark [text & image]
+- watermark padding
+- image watermark resize
+- image watermark opacity
+- TrueType Font text watermark size
+- TrueType Font text watermark colour
+- TrueType Font text watermark angle: 360 degree rotation
+
+
+## Supported File types
+
+| Images        | Others      |
+| ------------- |-------------|
+| jpg           | More to come|
+| gif           | &nbsp;      |
+| png           | &nbsp;      |
+
+
+## Supported Watermark Images
+| Images        | info      |
+| ------------- |-------------|
+| jpg           | |
+| gif           | alpha transparency supported|
+| png           | alpha transparency supported|
+
+## Supported Watermark Text
+As an alternative to a Watermark image, support is also available for TrueType Font watermarking with 360 degrees of rotation on the text.
+There are also methods to install and uninstall TrueType Fonts.
+
+
+## Basic Usage
+`$params` can be specified on `$wm = new Watermark($params);`
+<br>or can be set with setters `$wm->setInputFile($params['InputFile']);`
+<br>or optionally be sent on the `$wm->apply($params);` method
+<br>or a mixture of either.
+```
+include_once 'watermark.php';
+$params = array(
+  'InputFile' => 'images/image.jpg',
+);
+
+// Instanciate the Watermark Class
+$wm = new Watermark($params);
+
+// Start the Watermarking process
+$result = $wm->apply();
+
+// Return the watermarked Image path & filename
+$output = $wm->getOutput();
+```
+
+## Options
+Each option has a getter and setter, and can used by prefixing the option name with set or get respectively. E.G. to set the InputFile use `$wm->setInputFile()` and to get `$wm->getInputFile()`
+
+| Option Name |  Default | Type | Description |
+|-------------|----------|------|-------------|
+| InputFile   |         | string| The File that is to have the watermark applied              
+| OutputPath  | | string | Where the resulting file is to be saved. If not specified it will try to save in the same directory of the InputFile     
+| FontsPath  | ./watermark/fonts/ | string | Where the TrueType Fonts are located, relative to the Watermark Class.    
+| OutputFilePrefix  | "watermark" |string| Prefix for the output filename    
+| OutputFileOverwrite  | FALSE |boolean| Allow overwriting of existing files for the output
+| WmFont | "arial.ttf" |string| TrueType Font to Use |
+| WmFontSize | 12 |integer| Size of the TrueType Font Watermark. Minimum 10|
+| WmFontAngle | 0 |integer| Number of degrees to rotate the text, 0-360.<br>0 = Horizontal |
+| WmFontColour | "0,0,0" |string| Colour of the watermark text. RGB colour values. Valid values are 0-255. <br>Class Constants:<br>FONT_COLOUR_BLACK = "0,0,0"<br>FONT_COLOUR_GREY = '128,128,128'<br>FONT_COLOUR_WHITE = '255,255,255'|
+| WmPosition | 1 |integer| Position of the Watermark.<br>Class Constants:<br>POS_CENTERED = 1<br>POS_TOP_LEFT = 2<br>POS_TOP_CENTER = 3<br>POS_TOP_RIGHT = 4<br>POS_MIDDLE_LEFT = 5<br>POS_MIDDLE_RIGHT = 6<br>POS_BOTTOM_LEFT = 7<br>POS_BOTTOM_CENTER = 8<br>POS_BOTTOM_RIGHT  = 9|
+| WmPadding | 10 |integer| Padding around the watermark |
+| WmType | 2 |integer| Image Watermark or Text Watermark<br>Class Constants:<br>WM_TYPE_IMAGE = 1<br>WM_TYPE_TEXT = 2 |
+| WmText | "Sample Watermark" |string| The text string for the watermark |
+| WmImage |  |string| The File that is to be the watermark |
+| WmImageHeight | 100 |integer| Height of the Watermark Image, keeps aspect ratio in combination with WmImageWidth. Minimum 10. |
+| WmImageWidth | 100 |integer| Width of the Watermark Image, keeps aspect ratio in combination with WmImageHeight. Minimum 10. |
+| WmImageOpacity | 100 |integer| How opaque the watermark image is 0-100. 100 is Opaque, 0 is Transparent |
---
+++ watermark/image.php
@@ -0,0 +1,323 @@
+<?php
+/**
+ * Watermark Image Files of
+ * requires GD Library
+ *
+ *
+ * @author Alex Wright [alec@intrica.net]
+ * @version 1.0.0
+ * @copyright Copyright 2016, intrica.net
+ */
+class WatermarkImage
+{
+  /**
+   * @var Watermark container for the Watermark Class
+   */
+  private $Wm = FALSE;
+
+
+  public function __construct(Watermark $Wm)
+  {
+    $this->Wm = $Wm;
+  }
+
+  /**
+   * Process The Watermarking
+   */
+  public function process()
+  {
+    $ret = FALSE;
+    $isImageWatermark = (($this->Wm->getWmType() == Watermark::WM_TYPE_IMAGE) ? TRUE : FALSE);
+
+    // Load The Image
+    $imSource = $this->loadImage($this->Wm->getInputFileExt(), $this->Wm->getInputFile());
+
+    if ($isImageWatermark) {
+      $wmSource = $this->loadImage($this->Wm->getWmImageExt(), $this->Wm->getWmImage());
+      $wmSource = $this->resizeImageResource($wmSource, $this->Wm->getWmImageHeight(), $this->Wm->getWmImageWidth());
+      // Get watermark Size
+      $wmSizes = $this->getImageSize($wmSource);
+      // Get Watermark Position
+      $wmPosition = $this->getWmImagePosition($imSource, $this->Wm->getWmPosition(), $wmSizes, $this->Wm->getWmPadding());
+      // Apply watermark
+      $this->imagecopymerge_alpha($imSource, $wmSource, $wmPosition['x'], $wmPosition['y'], 0, 0, $wmSizes['width'], $wmSizes['height'], $this->Wm->getWmImageOpacity());
+      # Destroy temp images
+      imagedestroy($wmSource);
+    } else {
+      $colourParts = explode(",", $this->Wm->getWmFontColour());
+      $colour = imagecolorallocate($imSource, $colourParts[0], $colourParts[1], $colourParts[2]);
+      // Get Watermark Text Position
+      $wmPosition = $this->getWmTextPosition($imSource, $this->Wm->getWmPosition(), $this->Wm->getWmFontSize(), $this->Wm->getWmFontAngle(), $this->Wm->getFontPathFilename(), $this->Wm->getWmText(), $this->Wm->getWmPadding());
+      imagettftext($imSource, $this->Wm->getWmFontSize(), $this->Wm->getWmFontAngle(), $wmPosition['x'], $wmPosition['y'], $colour, $this->Wm->getFontPathFilename(), $this->Wm->getWmText());
+    }
+    // Save image
+    $ret = $this->saveImage($this->Wm->getInputFileExt(), $imSource, $this->Wm->getOutput());
+
+    # Destroy temp images
+    imagedestroy($imSource);
+    return $ret;
+  }
+
+  private function saveImage($ext, $imageResource, $filepath)
+  {
+    $ret = FALSE;
+    switch ($ext) {
+      case 'jpg':
+        $ret = imagejpeg($imageResource, $filepath, 100);
+        break;
+
+      case 'gif':
+        $ret = imagegif($imageResource, $filepath);
+        break;
+
+      case 'png':
+        $ret = imagepng($imageResource, $filepath, 0);
+        break;
+    }
+    return $ret;
+  }
+
+  private function loadImage($ext, $file)
+  {
+    $ret = FALSE;
+    switch ($ext) {
+      case 'jpg':
+        $ret = imagecreatefromjpeg($file);
+        break;
+
+      case 'gif':
+        $ret = imagecreatefromgif($file);
+        break;
+
+      case 'png':
+        $ret = imagecreatefrompng($file);
+        break;
+    }
+    return $ret;
+  }
+
+  /**
+   * Get the Widtha nd Height of an Image Resource
+   * @param mixed $img
+   * @return array [width,height]
+   */
+  private function getImageSize($img)
+  {
+    return array('width' => imagesx($img), 'height' => imagesy($img));
+  }
+
+
+  /**
+   * Get the Position for a Watermark Image
+   * @param mixed $imSource
+   * @param mixed $position
+   * @param mixed $wmSizes
+   * @param mixed $padding
+   * @return array [x,y] Position to place the waermark
+   */
+  private function getWmImagePosition($imSource, $position, $wmSizes, $padding)
+  {
+    $imSizes = $this->getImageSize($imSource);
+    $posX = 0;
+    $posY = 0;
+
+    switch ($position) {
+
+      # Centered
+      case 1:
+        $posX = ( $imSizes['width'] / 2 ) - ( $wmSizes['width'] / 2 );
+        $posY = ( $imSizes['height'] / 2 ) - ( $wmSizes['height'] / 2 );
+      break;
+
+      # Top-Left
+      case 2:
+        $posX = $padding;
+        $posY = $padding;
+      break;
+
+      # Top-Center
+      case 3:
+        $posX = (($imSizes['width'] - $wmSizes['width']) / 2);
+  			$posY = $padding;
+      break;
+
+      # Top-Right
+      case 4:
+        $posX = (($imSizes['width'] - $wmSizes['width']) - $padding);
+  			$posY = $padding;
+      break;
+
+      # Middle-Left
+      case 5:
+        $posX = $padding;
+  			$posY = (($imSizes['height'] / 2) - ($wmSizes['height'] / 2));
+      break;
+
+      # Middle-Right
+      case 6:
+        $posX = (($imSizes['width'] - $wmSizes['width']) - $padding);
+  			$posY = (($imSizes['height'] / 2) - ($wmSizes['height'] / 2));
+      break;
+
+      # Bottom-Left
+      case 7:
+        $posX = $padding;
+  			$posY = (($imSizes['height'] - $wmSizes['height']) - $padding);
+      break;
+
+      # Bottom-Center
+      case 8:
+        $posX = (($imSizes['width'] - $wmSizes['width']) / 2);
+			  $posY = (($imSizes['height'] - $wmSizes['height']) - $padding);
+      break;
+
+      # Bottom-Right
+      case 9:
+        $posX = (($imSizes['width'] - $wmSizes['width']) - $padding);
+  			$posY = (($imSizes['height'] - $wmSizes['height']) - $padding);
+      break;
+    }
+    return array('x' => $posX, 'y' => $posY);
+  }
+
+
+  /**
+   * Get the Position for a Watermark Text String
+   * @param mixed $imSource
+   * @param mixed $position
+   * @param mixed $fontSize
+   * @param mixed $fontAngle
+   * @param mixed $font
+   * @param mixed $text
+   * @param mixed $padding
+   */
+  private function getWmTextPosition($imSource, $position, $fontSize, $fontAngle, $font, $text, $padding)
+  {
+    $bbox = imagettfbbox($fontSize, $fontAngle, $font, $text);
+    $minX = min(array($bbox[0],$bbox[2],$bbox[4],$bbox[6]));
+    $maxX = max(array($bbox[0],$bbox[2],$bbox[4],$bbox[6]));
+    $minY = min(array($bbox[1],$bbox[3],$bbox[5],$bbox[7]));
+    $maxY = max(array($bbox[1],$bbox[3],$bbox[5],$bbox[7]));
+
+    // Bottom Left Corner of Text regardless of angle
+    $textX = $bbox[0];
+    $textY = $bbox[1];
+
+    // Dimensions of Container Box
+    $width = ($maxX - $minX);
+    $height = ($maxY - $minY);
+
+    // Top, Bottom, Left and Right Offsets
+    $offset = array(
+      't' => abs($textY - $minY),
+      'b' => abs($textY - $maxY),
+      'l' => abs($textX - $minX),
+      'r' => abs($textX - $maxX),
+    );
+
+    $imSizes = $this->getImageSize($imSource);
+    $posX = 0;
+    $posY = 0;
+
+    switch ($position) {
+
+      # Centered
+      case 1:
+        $posX = ((($imSizes['width'] / 2) - ($width / 2)) + $offset['l']);
+        $posY = ((($imSizes['height'] / 2) + ($height / 2)) - $offset['b']);
+      break;
+
+      # Top-Left
+      case 2:
+        $posX = ($offset['l'] + $padding);
+        $posY = ($offset['t'] + $padding);
+      break;
+
+      # Top-Center
+      case 3:
+        $posX = ((($imSizes['width'] - $width) / 2) + $offset['l']);
+        $posY = ($offset['t'] + $padding);
+
+      break;
+
+      # Top-Right
+      case 4:
+        $posX = ((($imSizes['width'] - $width) + $offset['l']) - $padding);
+  			$posY = ($offset['t'] + $padding);
+      break;
+
+      # Middle-Left
+      case 5:
+        $posX = ($offset['l'] + $padding);
+  			$posY = (($imSizes['height'] / 2) - ($height / 2) + $offset['t']);
+      break;
+
+      # Middle-Right
+      case 6:
+        $posX = ((($imSizes['width'] - $width) + $offset['l']) - $padding);
+  			$posY = (($imSizes['height'] / 2) - ($height / 2) + $offset['t']);
+      break;
+
+      # Bottom-Left
+      case 7:
+        $posX = ($offset['l'] + $padding);
+  			$posY = (($imSizes['height'] - $offset['b']) - $padding);
+      break;
+
+      # Bottom-Center
+      case 8:
+        $posX = ((($imSizes['width'] - $width) / 2) + $offset['l']);
+			  $posY = (($imSizes['height'] - $offset['b']) - $padding);
+      break;
+
+      # Bottom-Right
+      case 9:
+        $posX = ((($imSizes['width'] - $width) + $offset['l']) - $padding);
+  			$posY = (($imSizes['height'] - $offset['b']) - $padding);
+      break;
+    }
+
+    return array('x' => $posX, 'y' => $posY);
+  }
+
+  /**
+   * Resize an Image Resource
+   * @param mixed $imgResource Image resource to be resized
+   * @param int $newH
+   * @param int $newW
+   */
+  private function resizeImageResource($imgResource, $newH, $newW)
+  {
+    $ret = $imgResource;
+    $origSizes = $this->getImageSize($imgResource);
+    $ratio = ($origSizes['width'] / $origSizes['height']);
+
+    if (($newW / $newH) > $ratio) {
+       $newW = ($newH * $ratio);
+    } else {
+       $newH = ($newW / $ratio);
+    }
+
+    if ($NewImageResource = imagecreatetruecolor($newW, $newH)) {
+      imagealphablending($NewImageResource, false);
+      imagesavealpha($NewImageResource, true);
+      if (imagecopyresampled($NewImageResource, $imgResource, 0, 0, 0, 0, $newW, $newH, $origSizes['width'], $origSizes['height'])) {
+        $ret = $NewImageResource;
+      }
+    }
+
+    return $ret;
+  }
+
+  private function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct){
+    // creating a cut resource
+    $cut = imagecreatetruecolor($src_w, $src_h);
+    // copying relevant section from background to the cut resource
+    imagecopy($cut, $dst_im, 0, 0, $dst_x, $dst_y, $src_w, $src_h);
+    // copying relevant section from watermark to the cut resource
+    imagecopy($cut, $src_im, 0, 0, $src_x, $src_y, $src_w, $src_h);
+    // insert cut resource to destination image
+    imagecopymerge($dst_im, $cut, $dst_x, $dst_y, 0, 0, $src_w, $src_h, $pct);
+}
+
+}
